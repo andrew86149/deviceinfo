41e2259f61b1ce5e8c1a4c55310a4484e3450d8e
andrew86149 <35253420+andrew86149@users.noreply.github.com>
2020-11-04 10:51:11 +0300

    QuizActivity, CheatActivity, прередача данных между активити

--- app/src/main/java/ru/penza/aabr/deviceinfo/QuizActivity.java
+++ app/src/main/java/ru/penza/aabr/deviceinfo/QuizActivity.java
@@ -1,8 +1,13 @@
 package ru.penza.aabr.deviceinfo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,27 +15,42 @@ import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
+
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.hello_quiz,true),
             new Question(R.string.question_africa,false),
-            new Question(R.string.question_oceans,true)
+            new Question(R.string.question_oceans,true),
+            new Question(R.string.question_americas,true),
+            new Question(R.string.question_asia,true),
+            new Question(R.string.question_mideast,false)
     };
 
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
+
+        if (savedInstanceState!=null){
+            mCurrentIndex=savedInstanceState.getInt(KEY_INDEX,0);
+        }
 
         mTrueButton = findViewById(R.id.true_button);
         mFalseButton=findViewById(R.id.false_button);
         mNextButton=findViewById(R.id.next_button);
+        mCheatButton=findViewById(R.id.cheat_button);
         mQuestionTextView = findViewById(R.id.question);
 
         updateQuestion();
@@ -55,7 +75,19 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheater=false;
                 updateQuestion();
+            }
+        });
+
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent intent = new Intent(QuizActivity.this,CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].ismAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this,answerIsTrue);
+                //startActivity(intent);
+                startActivityForResult(intent,REQUEST_CODE_CHEAT);
             }
         });
     }
@@ -68,11 +100,66 @@ public class QuizActivity extends AppCompatActivity {
     private void checkAnswer(boolean userPressedTrue){
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].ismAnswerTrue();
         int messageResId=0;
-        if(userPressedTrue==answerIsTrue){
-            messageResId=R.string.correct_toast;
-        } else {
-            messageResId=R.string.incorrect_toast;
+        if (mIsCheater){
+            messageResId=R.string.judgment_toast;
+        }else {
+            if(userPressedTrue==answerIsTrue){
+                messageResId=R.string.correct_toast;
+            } else {
+                messageResId=R.string.incorrect_toast;
+            }
         }
         Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode!= Activity.RESULT_OK){
+            return;
+        }
+        if(requestCode==REQUEST_CODE_CHEAT){
+            if (data==null){
+                return;
+            }
+            mIsCheater=CheatActivity.wasAnswerShown(data);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG,"onSaveInstanceState");
+        outState.putInt(KEY_INDEX,mCurrentIndex);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy() called");
     }
 }
